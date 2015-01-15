404d360d86975bae71a2bef3383231f438364047
Theo Chatzimichos <tampakrap@gmail.com>
2015-01-15 14:29:45 +0100

    getmailpass.py: Add helper script/lib, that retrieves pwd of specified mail account

---
+++ getmailpass.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import keyring
+import socket
+import sys
+
+
+def get_passeval(account):
+    if account == 'gmail':
+        account = 'google'
+        service = 'tampakrap.gr/%s/mutt' % socket.gethostname()
+    elif account == 'gentoo':
+        service = 'various'
+    elif account == 'suse':
+        account = 'suse.de'
+        service = 'sites'
+    try:
+        passfile_content = keyring.get_password(service, account).split('\n')[0]
+        return passfile_content
+    except AttributeError:
+        raise Exception("GPG key expired")
+
+if __name__ == "__main__":
+    try:
+        print(get_passeval(sys.argv[1]))
+    except Exception, e:
+        sys.stderr.write(str(e))
+        sys.exit(1)
