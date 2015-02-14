6576a49338240fbb224f790f0823740281f9cd28
Theo Chatzimichos <tampakrap@gmail.com>
2015-02-14 23:13:19 +0100

    puppetfile-check.rb: Initial commit

---
+++ puppetfile-check.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+
+require 'colored'
+require 'puppet_forge'
+require 'r10k/puppetfile'
+
+puppetfile_path = '/etc/puppet/environments/production'
+puppetfile = R10K::Puppetfile.new(puppetfile_path).load
+user_agent = 'Tampakrap/0.1.0'
+
+modules = {}
+
+puppetfile.each do |pup_mod|
+  if pup_mod.class == R10K::Module::Forge
+
+    forge_version = PuppetForge::Module.find(pup_mod.title.gsub('/', '-')).current_release.version
+
+    modules[pup_mod.title] = [pup_mod.expected_version, forge_version]
+
+    if pup_mod.expected_version == forge_version
+      puts "#{pup_mod.title} is up to date"
+    else
+      puts "#{pup_mod.title} is OUTDATED: #{pup_mod.expected_version} vs #{forge_version}".red
+    end
+  end
+end
